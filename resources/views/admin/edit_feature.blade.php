4f55214ad7f5060399357b0be2f28f5bde323da2
Bahram Ahady <ba.ahady@gmail.com>
2018-07-24 20:25:28 +0430

    Feature edit is completed

---
+++ resources/views/admin/edit_feature.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-6">
+        <section class="panel">
+
+            @if(!is_null(Session::get('message')))
+            <div class="alert alert-success">
+                {{Session::get('message')}}
+            </div>
+            @endif
+
+            <header class="panel-heading">
+                ویرایش مشخصه
+            </header>
+            <div class="panel-body">
+                <form action="{{Route('feature.update',$feature->id)}}" method="post" role="form">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">ویرایش</label>
+                        <input type="text" name="title" class="form-control" value="{{$feature->title}}">
+                        @method('put')
+                        @csrf
+                    </div>
+                    <button type="submit" class="btn btn-info">ویرایش</button>
+                </form>
+            </div>
+        </section>
+    </div>
+</div>
+@endsection()
