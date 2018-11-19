f1939f3d971bcad7acae215b496a80de6d4419ff
MarshalllM <hamed_mogadam@yahoo.com>
2018-11-20 02:12:37 +0330

    Reset Password Page,Disable Register Api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -16,8 +16,8 @@ public $successStatus = 200;
     public function login(){ 
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
             $user = Auth::user(); 
-            $success['token'] =  $user->createToken('YadakApp')-> accessToken; 
-            return response()->json(['success' => $success], $this-> successStatus); 
+            $success['token'] =  $user->createToken('YadakApp')->accessToken; 
+            return response()->json(['success' => $success], $this->successStatus); 
         } 
         else{ 
             return response()->json(['error'=>'Unauthorised'], 401); 
@@ -28,24 +28,24 @@ public $successStatus = 200;
      * 
      * @return \Illuminate\Http\Response 
      */ 
-    public function register(Request $request) 
-    { 
-        $validator = Validator::make($request->all(), [ 
-            'name' => 'required', 
-            'email' => 'required|email', 
-            'password' => 'required', 
-            'c_password' => 'required|same:password', 
-        ]);
-if ($validator->fails()) { 
-            return response()->json(['error'=>$validator->errors()], 401);            
-        }
-$input = $request->all(); 
-        $input['password'] = bcrypt($input['password']); 
-        $user = User::create($input); 
-        $success['token'] =  $user->createToken('YadakApp')-> accessToken; 
-        $success['name'] =  $user->name;
-return response()->json(['success'=>$success], $this-> successStatus); 
-    }
+//     public function register(Request $request) 
+//     { 
+//         $validator = Validator::make($request->all(), [ 
+//             'name' => 'required', 
+//             'email' => 'required|email', 
+//             'password' => 'required', 
+//             'c_password' => 'required|same:password', 
+//         ]);
+// if ($validator->fails()) { 
+//             return response()->json(['error'=>$validator->errors()], 401);            
+//         }
+// $input = $request->all(); 
+//         $input['password'] = bcrypt($input['password']); 
+//         $user = User::create($input); 
+//         $success['token'] =  $user->createToken('YadakApp')->accessToken; 
+//         $success['name'] =  $user->name;
+// return response()->json(['success'=>$success], $this->successStatus); 
+//     }
 /** 
      * details api 
      * 
@@ -54,6 +54,6 @@ return response()->json(['success'=>$success], $this-> successStatus);
     public function details() 
     { 
         $user = Auth::user(); 
-        return response()->json(['success' => $user], $this-> successStatus); 
+        return response()->json(['success' => $user], $this->successStatus); 
     } 
 }
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Http\Request;
 
 
 Route::post('login', 'API\UserController@login');
-Route::post('register', 'API\UserController@register');
+// Route::post('register', 'API\UserController@register');
 Route::group(['middleware' => 'auth:api'], function(){
 Route::post('details', 'API\UserController@details');
 });
