ee99e83df8767ef6b71bca2d386c1f980680e518
MarshalllM <hamed_mogadam@yahoo.com>
2018-11-20 17:26:53 +0330

    API INDEX 5

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request; 
 use App\Http\Controllers\Controller; 
 use App\User; 
+use App\Product;
 use Illuminate\Support\Facades\Auth; 
 use Validator;
 class UserController extends Controller 
