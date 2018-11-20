750e3c27e3708e7cf61736ce355d748fc9eb6fee
MarshalllM <hamed_mogadam@yahoo.com>
2018-11-20 18:59:07 +0330

    API INDEX 9

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,10 @@ use Illuminate\Http\Request;
 
 
 Route::post('login', 'API\UserController@login');
+Route::post('index','API\UserController@index');
 // Route::post('register', 'API\UserController@register');
 Route::group(['middleware' => 'auth:api'], function(){
 Route::post('details', 'API\UserController@details');
-Route::post('index','API\UserController@index');
 });
 
 
