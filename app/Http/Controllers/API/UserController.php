5222d50ed2f91743279287f27bc57f5265407911
MarshalllM <hamed_mogadam@yahoo.com>
2018-11-20 21:46:23 +0330

    ugh13

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -27,7 +27,7 @@ public $successStatus = 200;
 
     public function index() 
     { 
-        $products = Product::select('id')->get();
+        $products = Product::select('id','full_body')->get();
 
 
 	return response()->json(['success' => $products], $this->successStatus); 
