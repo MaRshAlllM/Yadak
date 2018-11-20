602e4a20894716a1411baa47b758ec074b2e1b4e
MarshalllM <hamed_mogadam@yahoo.com>
2018-11-20 19:41:27 +0330

    ugh2

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -27,7 +27,7 @@ public $successStatus = 200;
 
     public function index() 
     { 
-        $products = Product::find(1);
+        $products = Product::find(4);
 
 
 	return response()->json(['success' => $products], $this->successStatus); 
