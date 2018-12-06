e2874ef4843238db919ab7aff1fbbd6c284c1c54
baahady <ba.ahady@gmail.com>
2018-12-06 12:48:35 +0330

    ADMIN - user list DESC order

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     public function index(User $user){
 
-        $users = $user->with('roles')->get();
+        $users = $user->with('roles')->orderby('created_at','DESC')->get();
 
         return view('admin.users.index')->with('users',$users);
     }
